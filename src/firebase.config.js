abced006acc8f67bcc6094e763969cdf233a49c2
karinawidya <karinawidya9999@gmail.com>
2020-04-16 11:18:56 +0700

    Praktikum dan Tugas Firebase React Login, Join With Google

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyA1gB8jlJv_KN4of2QUnafEBIlPZmOqz8Q",
+    authDomain: "pbf-firebase.firebaseapp.com",
+    databaseURL: "https://pbf-firebase.firebaseio.com",
+    projectId: "pbf-firebase",
+    storageBucket: "pbf-firebase.appspot.com",
+    messagingSenderId: "1083430758604",
+    appId: "1:1083430758604:web:784d35d2dd09f793767a69",
+    measurementId: "G-7T9WT87T4R"
+};
